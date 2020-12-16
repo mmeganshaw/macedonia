403b6b4eb75fe145fb5612a3bf69263749674a6b
Megan Shaw <mmeganshaw@gmail.com>
2020-12-15 23:05:23 -0500

    First commit. Setting packaging requirements and project structure.

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+module.exports = function (app) {
+
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/home.html"));
+    });
+
+    app.get("*", function (req, res) {
+        res.redirect('/');
+    });
+
+}
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/public'));
+
+require("./routes/index.js")(app);
+
+app.listen(PORT, function () {
+    console.log('listening on port ' + PORT)
+})
