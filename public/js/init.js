930bfe9d9e0e4c9521e915aa43c2328bfaa138a4
Megan Shaw <mmeganshaw@gmail.com>
2020-12-18 00:16:58 -0500

    Adding beginning website elements

---
+++ public/js/init.js
@@ -0,0 +1,12 @@
+(function ($) {
+    $(function () {
+
+        $('.sidenav').sidenav();
+        $('.carousel').carousel();
+        $('.collapsible').collapsible();
+        $('select').formSelect();
+        $('.modal').modal();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
