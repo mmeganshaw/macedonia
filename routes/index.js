a2209bf22442d46d52ea4be996806618e7806457
Megan Shaw <mmeganshaw@gmail.com>
2021-02-04 18:23:59 -0500

    Finishing home page.

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,22 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
 
+    app.get("/pricing", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/pricing.html"))
+    })
+
+    app.get("/location", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/location.html"))
+    })
+
+    app.get("/faqs", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/faqs.html"))
+    })
+
+    app.get("/contact", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/contact.html"))
+    })
+
     app.get("*", function (req, res) {
         res.redirect('/');
     });
